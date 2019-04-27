383f6903f13913d73f92c5180c5e7089d21c79ed
barrylee111 <barrylee115@gmail.com>
2019-04-27 17:31:44 -0600

    Updates

---
+++ precache-manifest.3735ed64fff633c4d3a2a304d381f285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38117b70a3162b84dba4ddade3e00d0",
+    "url": "/robofriends2/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends2/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "cfb6a195d1fa119d0c73",
+    "url": "/robofriends2/static/css/main.30c01f7c.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends2/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "cfb6a195d1fa119d0c73",
+    "url": "/robofriends2/static/js/main.b9afa3b7.chunk.js"
+  },
+  {
+    "revision": "c3f2d2f3a94738ae8e4c",
+    "url": "/robofriends2/static/js/runtime~main.cbd6455e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends2/static/media/SEGA.12a3db8b.woff"
+  }
+]);
