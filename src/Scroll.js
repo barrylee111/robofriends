bdccee6a09f601d88a64b0d77f13fee731b377be
barrylee111 <barrylee115@gmail.com>
2019-06-04 17:14:10 -0600

    Added redux

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 const Scroll = (props) => {
 	return(
-		<div style={{overflowY: 'scroll', border: '1px solid black', height: '500px'}}>
-			{props.children}
+		<div 
+			style={{overflow: 'scroll', border: '5px solid black', height: '800px'}}>
+				{props.children}
 		</div>
 	);
 };
